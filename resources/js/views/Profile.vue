f2841a89b11894be80cfce84e5cb70ccc6f5c757
Sébastien Merveille <info@sebastienmerv.be>
2024-10-30 01:45:58 +0100

    Update pp

--- resources/js/views/Profile.vue
+++ resources/js/views/Profile.vue
@@ -13,7 +13,7 @@
                         <div class="col-span-full">
                             <label for="about" class="block text-sm/6 font-medium text-gray-900">About</label>
                             <div class="mt-2">
-                                <textarea id="about" name="about" rows="3" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                <textarea id="about" name="about" rows="3" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                             <p class="mt-3 text-sm/6 text-gray-600">{{ $t('admin.about_description') }}</p>
                         </div>
@@ -22,7 +22,9 @@
                             <label for="photo" class="block text-sm/6 font-medium text-gray-900">{{ $t('admin.photo') }}</label>
                             <div class="mt-2 flex items-center gap-x-3">
                                 <UserCircleIcon class="h-12 w-12 text-gray-300" aria-hidden="true" v-if="!user.profile_photo_path" />
-                                <img v-else :src="user.profile_photo_path" alt="Profile picture" class="object-cover h-12 w-12 rounded-full" />
+                                <img
+                                    :key="user.profile_photo_path"
+                                    v-else :src="user.profile_photo_path" alt="Profile picture" class="object-cover h-12 w-12 rounded-full" />
                                 <label for="file" class="cursor-pointer rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
                                     {{$t('admin.change_picture')}}</label>
                                 <input type="file" id="file" name="file" class="hidden" accept="image/*" v-on:change="updateProfilePicture($event)" />
@@ -42,7 +44,7 @@
                             <div class="mt-2">
                                 <input
                                     v-model="user.firstName"
-                                    type="text" name="first-name" id="first-name" autocomplete="given-name" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                    type="text" name="first-name" id="first-name" autocomplete="given-name" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                         </div>
 
@@ -51,7 +53,7 @@
                             <div class="mt-2">
                                 <input
                                     v-model="user.lastName"
-                                    type="text" name="last-name" id="last-name" autocomplete="family-name" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                    type="text" name="last-name" id="last-name" autocomplete="family-name" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                         </div>
 
@@ -60,7 +62,7 @@
                             <div class="mt-2">
                                 <input
                                     v-model="user.email"
-                                    id="email" name="email" type="email" autocomplete="email" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                    id="email" name="email" type="email" autocomplete="email" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                         </div>
 
@@ -69,7 +71,7 @@
                             <div class="mt-2">
                                 <select
                                     v-model="user.country"
-                                    id="country" name="country" autocomplete="country-name" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm/6">
+                                    id="country" name="country" autocomplete="country-name" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:max-w-xs sm:text-sm/6">
                                     <option v-for="country in countries" :key="country">{{ country }}</option>
                                 </select>
                             </div>
@@ -80,7 +82,7 @@
                             <div class="mt-2">
                                 <input
                                     v-model="user.address"
-                                    type="text" name="street-address" id="street-address" autocomplete="street-address" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                    type="text" name="street-address" id="street-address" autocomplete="street-address" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                         </div>
 
@@ -89,7 +91,7 @@
                             <div class="mt-2">
                                 <input
                                     v-model="user.city"
-                                    type="text" name="city" id="city" autocomplete="address-level2" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                    type="text" name="city" id="city" autocomplete="address-level2" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                         </div>
 
@@ -98,7 +100,7 @@
                             <div class="mt-2">
                                 <input
                                     v-model="user.state"
-                                    type="text" name="region" id="region" autocomplete="address-level1" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                    type="text" name="region" id="region" autocomplete="address-level1" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                         </div>
 
@@ -107,7 +109,7 @@
                             <div class="mt-2">
                                 <input
                                     v-model="user.postal_code"
-                                    type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm/6" />
+                                    type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="px-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm/6" />
                             </div>
                         </div>
                     </div>
@@ -115,7 +117,7 @@
             </div>
             <div class="mt-6 flex items-center justify-end gap-x-6">
                 <button type="button" class="text-sm/6 font-semibold text-gray-900">{{ $t('cancel') }}</button>
-                <button type="submit" class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">{{$t('save')}}</button>
+                <button type="submit" class="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">{{$t('save')}}</button>
             </div>
         </form>
     </AdminView>
